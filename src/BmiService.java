4330efe789d4d8bb12b7ef3d0827b08b72067913
Светлана Алексеева <Lanaleks90@gmail.com>
2022-11-25 18:26:10 +0400

    Initial commit

---
+++ src/BmiService.java
@@ -0,0 +1,10 @@
+public class BmiService {
+
+    public double calculate(int weight, double height) {
+
+        double bmi = weight / (height * height);
+        {
+        }
+        return bmi;
+    }
+}
